e5fcd865aa8c92dee736c308dcccb2dc3587f559
Titus Wormer <tituswormer@gmail.com>
2021-04-05 22:43:41 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var complexWordWeight = 100
 var weight = 0.4
 
+/**
+ * @typedef {Object.<string, number>} GunningFogCounts
+ * @property {number} sentence
+ * @property {number} word
+ */
+
+/**
+ * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of complex (excluding jargon, proper nouns, compound words) polysillabic (three or more syllables) words (`complexPolysillabicWord`) in a document, returns the grade level associated with the document.
+ *
+ * @param {GunningFogCounts} counts
+ * @returns {number}
+ */
 export function gunningFog(counts) {
   if (!counts || !counts.sentence || !counts.word) {
     return Number.NaN
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import test from 'tape'
 import {gunningFog} from './index.js'
 
 test('gunningFog', function (t) {
+  // @ts-ignore
   t.ok(Number.isNaN(gunningFog()), 'NaN when an invalid value is given')
 
   t.equal(round(gunningFog({sentence: 1, word: 13})), 5.2)
@@ -14,6 +15,10 @@ test('gunningFog', function (t) {
   t.end()
 })
 
+/**
+ * @param {number} value
+ * @returns {number}
+ */
 function round(value) {
   return Math.round(value * 1e6) / 1e6
 }
