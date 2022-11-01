fd19dc060a9552f77bf894d5ce7155ff1ecb841c
Titus Wormer <tituswormer@gmail.com>
2022-11-01 15:15:31 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {gunningFog} from './index.js'
 
-test('gunningFog', function (t) {
-  // @ts-ignore
-  t.ok(Number.isNaN(gunningFog()), 'NaN when an invalid value is given')
+test('gunningFog', function () {
+  // @ts-expect-error
+  assert.ok(Number.isNaN(gunningFog()), 'NaN when an invalid value is given')
 
-  t.equal(round(gunningFog({sentence: 1, word: 13})), 5.2)
+  assert.equal(round(gunningFog({sentence: 1, word: 13})), 5.2)
 
-  t.equal(
+  assert.equal(
     round(gunningFog({sentence: 1, word: 13, complexPolysillabicWord: 2})),
     11.353_846
   )
-
-  t.end()
 })
 
 /**
